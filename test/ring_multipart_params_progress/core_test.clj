5c5920b82244f26c9489f976fd5ca67914ad78d0
Alexander Hudek <alex@kirasystems.com>
2015-07-28 15:17:27 -0400

    Initial commit.

---
+++ test/ring_multipart_params_progress/core_test.clj
@@ -0,0 +1,7 @@
+(ns ring-multipart-params-progress.core-test
+  (:require [clojure.test :refer :all]
+            [ring-multipart-params-progress.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
